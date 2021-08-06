a72e2d52f168cfd4fea8b563b60f72faf6dd039f
Lucio Fonseca <lucio.fonseca@goflink.com>
2021-08-06 10:38:53 +0200

    added nsobject subclass

--- Sources/SegmentAdjust/AdjustIntegrationFactory.swift
+++ Sources/SegmentAdjust/AdjustIntegrationFactory.swift
@@ -1,13 +1,9 @@
 import Foundation
 import Segment
 
-public class AdjustIntegrationFactory: SEGIntegrationFactory {
+public class AdjustIntegrationFactory: NSObject, SEGIntegrationFactory {
     public static let instance = AdjustIntegrationFactory()
-    
-    public init() {
         
-    }
-    
     public func create(withSettings settings: [AnyHashable : Any], for analytics: Analytics) -> Integration {
         return AdjustIntegration(settings: settings, analytics: analytics)
     }
