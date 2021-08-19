dfec7b110a035005d0eec31047927c95a0df0219
Lucio Fonseca <lucio.fonseca@goflink.com>
2021-08-19 16:25:11 +0200

    added Segment as framework

--- Package.swift
+++ Package.swift
@@ -10,18 +10,23 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "SegmentAdjust",
-            targets: ["SegmentAdjust"]),
+            targets: ["SegmentAdjust", "Segment"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         .package(name: "Adjust", url: "https://github.com/adjust/ios_sdk", from: "4.27.1"),
-        .package(name: "Segment", url: "https://github.com/segmentio/analytics-ios", from: "4.1.5"),
+//        .package(name: "Segment", url: "git@github.com:segmentio/analytics-ios.git", from: "4.1.5"),
+//        .package(
+//                    name: "Segment",
+//            url: "git@github.com:segmentio/analytics-ios.git", from: "4.1.5"
+//                )
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "SegmentAdjust",
-            dependencies: ["Segment", "Adjust"]),
+            dependencies: ["Adjust"]),
+        .binaryTarget(name: "Segment", path: "./Segment.xcframework")
     ]
 )
--- Sources/SegmentAdjust/AdjustIntegration.swift
+++ Sources/SegmentAdjust/AdjustIntegration.swift
@@ -100,7 +100,7 @@ public class AdjustIntegration: SegmentIntegration {
 //            }
 //        }
     }
-    
+        
     public override func flush() {
         
     }
